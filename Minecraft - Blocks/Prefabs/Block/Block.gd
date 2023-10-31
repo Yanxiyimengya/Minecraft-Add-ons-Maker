f1a6b0b5184a33e4fcb454cf9ad500acabcfe1a3
Yanxiyimengym <193446537@qq.com>
2023-11-01 03:17:45 +0800

    main

--- Minecraft - Blocks/Prefabs/Block/Block.gd
+++ Minecraft - Blocks/Prefabs/Block/Block.gd
@@ -3,49 +3,49 @@ extends Node2D;
 @export var attribute:BlockAttribute = BlockAttribute.new();
 @onready var sprite:Sprite2D = $BlockSprite;
 
-var frame:int = 0; # 当前帧
+var next_frame:int = 0;
 var frame_index:int = 0: # 当前播放的帧的序号
 	set(_val):
 		frame_index = _val;
+		next_frame = _val + 1;
+		if (!attribute.sprite_texturts.animation.frames_sort.is_empty()) :
+			if (next_frame >= attribute.sprite_texturts.animation.frames_sort.size()) :
+				next_frame = 0;
+			next_frame = attribute.sprite_texturts.animation.frames_sort[frame_index];
+		else:
+			if (next_frame >= attribute.sprite_texturts.animation.texture_count) :
+				next_frame = 0;
+		sprite.texture = attribute.sprite_texturts.get_frame_texture(frame_index);
+		sprite.material.set_shader_parameter("next_texture",attribute.sprite_texturts.get_frame_texture(next_frame));
+		sprite.material.set_shader_parameter("lerp", animation_lerp);
 var animation_timer:int = 0; # 动画播放计时器
-var animation_loop:float = 0; # 帧补间过渡值
+var animation_lerp:float = 0; # 帧补间过渡值
 
 var shader_uniforms;
 var shader:Shader:
 	set(_val):
 		sprite.material.shader = _val;
-		shader_uniforms = sprite.material.shader.get_shader_uniform_list();
 	get:
 		return sprite.material.shader;
 func _ready():
 	shader = sprite.material.shader;
-	frame = attribute.sprite_texturts.animation.frame_first;
-	sprite.texture = attribute.sprite_texturts.get_frame_texture(frame);
+	sprite.texture = attribute.sprite_texturts.get_frame_texture(attribute.sprite_texturts.animation.frame_first);
 	
 
 func _process(delta):
-	sprite.texture = attribute.sprite_texturts.get_frame_texture(frame);
-	
-	
-	if (attribute.sprite_texturts.animation.interpolate) :
-		shader_uniforms["lerp"] = 0.5;
-		shader_uniforms["next_texture"] = attribute.sprite_texturts.get_frame_texture(frame+1);
+	if (!attribute.sprite_texturts.animation.interpolate) :
+		animation_lerp = get_viewport().get_mouse_position().x/get_viewport().size.x;
+		print(animation_lerp);
 	
 	if (attribute.sprite_texturts.animation.texture_count > 1) :
 		if (animation_timer >= attribute.sprite_texturts.animation.frames_speed):
-			frame_index += 1;
-			if (!attribute.sprite_texturts.animation.frames_sort.is_empty()) :
-				if (frame_index >= attribute.sprite_texturts.animation.frames_sort.size()) :
-					frame_index = 0;
-				frame = attribute.sprite_texturts.animation.frames_sort[frame_index];
-			else:
-				if (frame_index >= attribute.sprite_texturts.animation.texture_count) :
-					frame_index = 0;
-				frame = frame_index;
+			frame_index = 1;
 			animation_timer = 0;
 		else :
 			animation_timer += 1;
-
+	if (Input.is_key_pressed(KEY_SPACE)):
+		$Sprite2D.texture = sprite.material.get_shader_parameter("next_texture")
+		print($Sprite2D.texture);
 func put() :
 	pass; # [提供重写]放置方法
 
